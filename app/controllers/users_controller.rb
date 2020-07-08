eee1777240d3d922cc472d0130f5f387b8945886
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-07-08 22:21:07 +0900

    CRUD API の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,45 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
+
+  def create
+    # インスタンスを model から作成する
+    @user = User.new(user_params)
+
+    # インスタンスを DB に保存する
+    @user.save!
+
+    # json として値を返す
+    render :show
+  end
+
+  def update
+    # 探してきたレコードに対して変更を行う
+    @user.update!(user_params)
+    # json として値を返す
+    render :show
+  end
+
+  def destroy
+    # 探してきたレコードを削除する
+    @user.destroy!
+    # json として値を返す
+    render :show
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :account, :email)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
